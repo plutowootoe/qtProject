c35ced2699c0099118899b450b358de7f578be90
Adam Greenan <87552818+plutowootoe@users.noreply.github.com>
2023-03-24 19:33:53 +0000

    Add files via upload
    added more functions to recipe.cpp

--- recipe.cpp
+++ recipe.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
-
+string name;
+vector<string> ingredients;
+string description = "";
+int calories = 0;
+int rating = 0;
 
 class recipe{
     public:
@@ -11,44 +15,54 @@ class recipe{
         string name;
         vector<string> ingredients;
         string description = "";
+        int calories = 0;
         int rating = 0;
         cout<<"Recipe Added:"<<name<<ingredients<<description<<rating<<endl;
-        virtual ~recipe(){
+}
+        ~recipe(){
             cout<<"Recipe Deleted"<<endl;
         }
-}
 
 
-recipe(ingredients,description);
+
+recipe(ingredients,calories,description,rating);
 
 
 void getIngredients(){
-    return ingredients;
+    return this->ingredients;
 
 }
 
-// adds an ingredients to the
-void AddIngredients(vector<string> input){
+// adds an ingredient to the ingredient list
+void addIngredients(vector<string> input){
    for(int i = 0;i<input.size();i++){
-       ingredients.pushback(input);
+       ingredients.pushback(input.at(i));
    }
 }
 
 
-// needs to be fixed
-void RemoveIngredient(string input){
-    for(int i = 0;i<getIngredients();i++){
-        if(input==ingredients{
-            ingredients.erase(ingredients.begin()+i)
+// needs to be fixed // removes ingredient from list
+void removeIngredient(vector<string> input){
+    for(int i = 0;i<input.size();i++){
+        if(input==ingredients.at(i)){
+            ingredients.erase(ingredients.begin()+i);
+        }
+        else{
+            i++;
         }
     }
    }
-void EditRecipe(){
-    return 0;
+void getCalories(){
+    return this->calories;
 }
 
+void getDescription(){
+    return this->description;
+}
 
-
+void getRating(){
+    return this->rating;
+}
 
 
 
